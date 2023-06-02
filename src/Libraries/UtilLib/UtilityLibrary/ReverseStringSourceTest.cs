636cd01f003a6e510f4584127b87a64bca81ab28
Oğuz Karan <oguzkaran@csystem.org>
2023-06-02 23:49:18 +0300

    Update

--- src/Libraries/UtilLib/UtilityLibrary/ReverseStringSourceTest.cs
+++ src/Libraries/UtilLib/UtilityLibrary/ReverseStringSourceTest.cs
@@ -1,24 +1,16 @@
 ﻿namespace UtilityLibrary;
 
 [TestFixture]
-public class Tests
+public class ReverseStringSourceTest
 {
-    private static IEnumerable<string[]> SourceSupplier()
+    public static IEnumerable<string[]> SourceSupplier()
     {
         yield return new string[] { "ankara", "arakna" };
         yield return new string[] { "alipapila", "alipapila" };
         yield return new string[] { "", "" };
     }
 
-    [Author("Ahmet Taçkın")]
-    [TestCase("ankara", "arakna")]
-    [TestCase("alipapila", "alipapila")]
-    [TestCase("", "")]
-    public void GivenString_WhenText_ThenReturnReversed(string input, string expected)
-    {
-        Assert.That(expected, Is.EqualTo(StringUtil.ReverseString(input)));
-    }
-
+   
     
     [Author("Ahmet Taçkın")]
     [Test]
---
+++ src/Libraries/UtilLib/UtilityLibrary/ReverseStringTest.cs
@@ -0,0 +1,14 @@
+﻿namespace UtilityLibrary;
+
+[TestFixture]
+public class ReverseStringTest
+{
+    [Author("Ahmet Taçkın")]
+    [TestCase("ankara", "arakna")]
+    [TestCase("alipapila", "alipapila")]
+    [TestCase("", "")]
+    public void GivenString_WhenText_ThenReturnReversed(string input, string expected)
+    {
+        Assert.That(expected, Is.EqualTo(StringUtil.ReverseString(input)));
+    }
+}
