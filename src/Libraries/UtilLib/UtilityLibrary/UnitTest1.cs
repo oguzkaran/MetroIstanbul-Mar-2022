55636c4be4c297727c544360d1a3bee5de5fdb1c
Oğuz Karan <oguzkaran@csystem.org>
2023-06-02 23:32:15 +0300

    Update

--- src/Libraries/UtilLib/UtilityLibrary/UnitTest1.cs
+++ src/Libraries/UtilLib/UtilityLibrary/UnitTest1.cs
@@ -1,26 +1,30 @@
 ﻿namespace UtilityLibrary;
 
+[TestFixture]
 public class Tests
 {
-    public static IEnumerable<string[]> SourceSupplier()
+    private static IEnumerable<string[]> SourceSupplier()
     {
         yield return new string[] { "ankara", "arakna" };
         yield return new string[] { "alipapila", "alipapila" };
         yield return new string[] { "", "" };
     }
 
+    [Author("Ahmet Taçkın")]
     [TestCase("ankara", "arakna")]
     [TestCase("alipapila", "alipapila")]
     [TestCase("", "")]
     public void GivenString_WhenText_ThenReturnReversed(string input, string expected)
     {
-        Assert.AreEqual(expected, StringUtil.ReverseString(input));
+        Assert.That(expected, Is.EqualTo(StringUtil.ReverseString(input)));
     }
 
+    
     [Author("Ahmet Taçkın")]
+    [Test]
     [TestCaseSource(nameof(SourceSupplier))]
-    public void GivenString_WhenText_ThenReturnReversed_Source(string input, string expected)
+    public void GivenString_WhenText_ThenReturnReversed_SourceSupplier(string input, string expected)
     {
-        Assert.AreEqual(expected, StringUtil.ReverseString(input));
+        Assert.That(expected, Is.EqualTo(StringUtil.ReverseString(input)));
     }
 }
