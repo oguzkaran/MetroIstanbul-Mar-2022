ae94a81e44c323cbc5abc291a1d7553dd7714402
Oğuz Karan <oguzkaran@csystem.org>
2023-09-19 22:26:45 +0300

    Update

--- src/SampleCodes/Sample/Program.cs
+++ src/SampleCodes/Sample/Program.cs
@@ -23,15 +23,10 @@ namespace CSD
             X = x;
             Y = y;
         }
-        public double Distance(int x = 0, int y = 0) => Sqrt(Pow(X - x, 2) + Pow(Y - y, 2));
-<<<<<<< HEAD
 
-=======
-       
->>>>>>> 01f4c4a2ce54d3b16c9af99cac19799ff72b77a6
+        public double Distance(int x = 0, int y = 0) => Sqrt(Pow(X - x, 2) + Pow(Y - y, 2));
 
         public double Distance(Point other) => Distance(other.X, other.Y);
-
 
         public override string ToString() => $"({X}, {Y}";
 
